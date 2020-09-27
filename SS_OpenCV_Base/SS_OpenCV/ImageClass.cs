e0b973b9fd73cc5aaa52b98bc3b730d108792032
brun0vieira <brunorebelovieira@gmail.com>
2020-09-27 14:54:35 +0100

    commit1

---
+++ SS_OpenCV_Base/SS_OpenCV/ImageClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace SS_OpenCV
+{
+    class ImageClass
+    {
+
+        /// <summary>
+        /// Image Negative using EmguCV library
+        /// Slower method
+        /// </summary>
+        /// <param name="img">Image</param>
+        public static void Negative(Image<Bgr, byte> img)
+        {
+            int x, y;
+
+            Bgr aux;
+            for (y = 0; y < img.Height; y++)
+            {
+                for (x = 0; x < img.Width; x++)
+                {
+                    // acesso directo : mais lento 
+                    aux = img[y, x];
+                    img[y, x] = new Bgr(255 - aux.Blue, 255 - aux.Green, 255 - aux.Red);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert to gray
+        /// Direct access to memory - faster method
+        /// </summary>
+        /// <param name="img">image</param>
+        public static void ConvertToGray(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+                byte blue, green, red, gray;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.nChannels; // number of channels - 3
+                int padding = m.widthStep - m.nChannels * m.width; // alinhament bytes (padding)
+                int x, y;
+
+                if (nChan == 3) // image in RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //retrive 3 colour components
+                            blue = dataPtr[0];
+                            green = dataPtr[1];
+                            red = dataPtr[2];
+
+                            // convert to gray
+                            gray = (byte)Math.Round(((int)blue + green + red) / 3.0);
+
+                            // store in the image
+                            dataPtr[0] = gray;
+                            dataPtr[1] = gray;
+                            dataPtr[2] = gray;
+
+                            // advance the pointer to the next pixel
+                            dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+        }
+
+
+    }
+}

4992fa66ce6d64f4e938829e4217210d2a28987d
brun0vieira <brunorebelovieira@gmail.com>
2020-10-07 15:14:37 +0100

    meanNotZero

--- SS_OpenCV_Base/SS_OpenCV/ImageClass.cs
+++ SS_OpenCV_Base/SS_OpenCV/ImageClass.cs
@@ -563,5 +563,53 @@ namespace SS_OpenCV
                 }
             }
         }
+
+        public static void Mean(Image<Bgr, byte> img, Image<Bgr, byte> imgCopy)
+        {
+            unsafe
+            {
+                MIplImage m = imgCopy.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer();
+
+                MIplImage m_dest = img.MIplImage;
+                byte* dataPtr_dest = (byte*)m_dest.imageData.ToPointer();
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChannels = m.nChannels;
+                int widthstep = m.widthStep;
+                int padding = m.widthStep - m.nChannels * m.width;
+                int x, y;
+                double blue_mean, green_mean, red_mean;
+
+                if(nChannels == 3)
+                {
+                    dataPtr += widthstep + nChannels;
+                    dataPtr_dest += widthstep + nChannels;
+
+                    // interior da imagem
+                    for(y = 1; y < height - 1; y++)
+                    {
+                        for(x = 1; x < width - 1; x++)
+                        {
+
+                            blue_mean = (dataPtr[0] + (dataPtr + nChannels)[0] + (dataPtr - nChannels)[0] + (dataPtr + widthstep)[0] + (dataPtr - widthstep)[0] + (dataPtr - nChannels - widthstep)[0] + (dataPtr - nChannels + widthstep)[0] + (dataPtr + nChannels - widthstep)[0] + (dataPtr + nChannels + widthstep)[0])/9.0;
+                            green_mean = (dataPtr[1] + (dataPtr + nChannels)[1] + (dataPtr - nChannels)[1] + (dataPtr + widthstep)[1] + (dataPtr - widthstep)[1] + (dataPtr - nChannels - widthstep)[1] + (dataPtr - nChannels + widthstep)[1] + (dataPtr + nChannels - widthstep)[1] + (dataPtr + nChannels + widthstep)[1])/9.0;
+                            red_mean = (dataPtr[0] + (dataPtr + nChannels)[2] + (dataPtr - nChannels)[2] + (dataPtr + widthstep)[2] + (dataPtr - widthstep)[2] + (dataPtr - nChannels - widthstep)[2] + (dataPtr - nChannels + widthstep)[2] + (dataPtr + nChannels - widthstep)[2] + (dataPtr + nChannels + widthstep)[2])/9.0;
+
+
+                            dataPtr_dest[0] = (byte)Math.Round(blue_mean);
+                            dataPtr_dest[1] = (byte)Math.Round(green_mean);
+                            dataPtr_dest[2] = (byte)Math.Round(red_mean);
+
+                            dataPtr += nChannels;
+                            dataPtr_dest += nChannels;
+                        }
+                        dataPtr += padding + 2 * nChannels;
+                        dataPtr_dest += padding + 2 * nChannels;
+                    }
+                }
+            }
+        }
     }
 }
